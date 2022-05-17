35fb50eefcb805471f1d0bbc5f4042a5320a3fca
ariwidiantara1 <aridevian99@gmail.com>
2022-05-17 12:14:54 +0800

    add fitur Dark Mode

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ window.onscroll = function () {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Pindahkan Posisi toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
